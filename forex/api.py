eb6ac7e186a0a13631086237dde6fa8a430c70bd
Ubuntu <ubuntu@ip-172-31-18-97.ap-south-1.compute.internal>
2018-01-02 07:50:01 +0000

    Validations before duty drawback entry

--- forex/api.py
+++ forex/api.py
@@ -1,5 +1,7 @@
 import frappe
 from frappe import _
+from frappe.contacts.doctype.address.address import get_address_display, get_default_address
+from frappe.contacts.doctype.contact.contact import get_contact_details, get_default_contact
 
 @frappe.whitelist()
 def payment_on_submit(self, method):
@@ -25,33 +27,43 @@ def si_on_cancel(self, method):
 	cancel_jv(self, method)
 	
 def create_jv(self, method):
-	jv = frappe.new_doc("Journal Entry")
-	jv.voucher_type = "Duty Drawback Entry"
-	jv.posting_date = self.posting_date
-	jv.company = self.company
-	jv.cheque_no = self.name
-	jv.cheque_date = self.posting_date
-	jv.user_remark = "Duty draw back against" + self.name + " for " + self.customer
-	jv.append("accounts", {
-		"account": frappe.db.get_value("Company", self.company, 'duty_drawback_receivable_account'),
-		"cost_center": "Export - BE",
-		"debit_in_account_currency": self.total_duty_drawback
-	})
-	jv.append("accounts", {
-		"account": frappe.db.get_value("Company", self.company, 'duty_drawback_income_account'),
-		"cost_center": "Export - BE",
-		"credit_in_account_currency": self.total_duty_drawback
-	})
-	jv.save(ignore_permissions=True)
-	jv.submit()
-	self.duty_drawback_jv = jv.name
-	self.save(ignore_permissions=True)
-	frappe.db.commit()
+	if self.currency != "INR":
+		if self.total_duty_drawback:
+			drawback_receivable_account = frappe.db.get_value("Company", { "company_name": self.company}, "duty_drawback_receivable_account")
+			drawback_income_account = frappe.db.get_value("Company", { "company_name": self.company}, "duty_drawback_income_account")
+			if not drawback_receivable_account:
+				frappe.throw(_("Set Duty Drawback Receivable Account in Company"))
+			elif not drawback_income_account:
+				frappe.throw(_("Set Duty Drawback Income Account in Company"))
+			else:
+				jv = frappe.new_doc("Journal Entry")
+				jv.voucher_type = "Duty Drawback Entry"
+				jv.posting_date = self.posting_date
+				jv.company = self.company
+				jv.cheque_no = self.name
+				jv.cheque_date = self.posting_date
+				jv.user_remark = "Duty draw back against" + self.name + " for " + self.customer
+				jv.append("accounts", {
+					"account": drawback_receivable_account,
+					"cost_center": "Export - BE",
+					"debit_in_account_currency": self.total_duty_drawback
+				})
+				jv.append("accounts", {
+					"account": drawback_income_account,
+					"cost_center": "Export - BE",
+					"credit_in_account_currency": self.total_duty_drawback
+				})
+				jv.save(ignore_permissions=True)
+				jv.submit()
+				self.duty_drawback_jv = jv.name
+				self.save(ignore_permissions=True)
+				frappe.db.commit()
 	
 def cancel_jv(self, method):
-	jv = frappe.get_doc("Journal Entry", self.duty_drawback_jv)
-	jv.cancel()
-	frappe.db.commit()
+	if self.duty_drawback_jv:
+		jv = frappe.get_doc("Journal Entry", self.duty_drawback_jv)
+		jv.cancel()
+		frappe.db.commit()
 
 #On Submit Payment
 def fwd_uti(self, method):
@@ -115,3 +127,67 @@ def cover_po(self, method):
 		po.amount_unhedged = so.grand_total - row.amount_covered
 		po.save()
 		frappe.db.commit()
+
+# Address and Contact
+@frappe.whitelist()
+def get_party_details(party=None, party_type="Customer", ignore_permissions=False):
+
+	if not party:
+		return {}
+
+	if not frappe.db.exists(party_type, party):
+		frappe.throw(_("{0}: {1} does not exists").format(party_type, party))
+
+	return _get_party_details(party, party_type, ignore_permissions)
+
+def _get_party_details(party=None, party_type="Customer", ignore_permissions=False):
+
+	out = frappe._dict({
+		party_type.lower(): party
+	})
+
+	party = out[party_type.lower()]
+
+	if not ignore_permissions and not frappe.has_permission(party_type, "read", party):
+		frappe.throw(_("Not permitted for {0}").format(party), frappe.PermissionError)
+
+	party = frappe.get_doc(party_type, party)
+	
+	set_address_details(out, party, party_type)
+	set_contact_details(out, party, party_type)
+	set_other_values(out, party, party_type)
+
+	return out
+
+def set_address_details(out, party, party_type):
+	billing_address_field = "customer_address" if party_type == "Lead" \
+		else party_type.lower() + "_address"
+	out[billing_address_field] = get_default_address(party_type, party.name)
+	
+	# address display
+	out.address_display = get_address_display(out[billing_address_field])
+
+def set_contact_details(out, party, party_type):
+	out.contact_person = get_default_contact(party_type, party.name)
+
+	if not out.contact_person:
+		out.update({
+			"contact_person": None,
+			"contact_display": None,
+			"contact_email": None,
+			"contact_mobile": None,
+			"contact_phone": None,
+			"contact_designation": None,
+			"contact_department": None
+		})
+	else:
+		out.update(get_contact_details(out.contact_person))
+
+def set_other_values(out, party, party_type):
+	# copy
+	if party_type=="Customer":
+		to_copy = ["customer_name", "customer_group", "territory", "language"]
+	else:
+		to_copy = ["supplier_name", "supplier_type", "language"]
+	for f in to_copy:
+		out[f] = party.get(f)
